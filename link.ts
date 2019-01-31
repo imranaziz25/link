20b3783bce9bea05d03d7d5581227a86a663db3a
imranaziz25 <47127324+imranaziz25@users.noreply.github.com>
2019-01-31 16:12:26 +0000

    Add files via upload

---
+++ link.ts
@@ -0,0 +1,14 @@
+export class Link {
+	private id: number;
+	private userid: number;
+	private title: string;
+  constructor(id: number, userid: number, title: string) 
+   { 
+     this.id = id; 
+	 this.userid = userid;
+	 this.title = title; 
+   }	
+   
+  
+    
+}
